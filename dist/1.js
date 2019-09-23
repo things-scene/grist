7e82dd9ed880795be61678afa3037af938db48e8
lllleeee <lcj1123@hatiolab.com>
2019-09-23 14:57:22 +0900

    ignore list 수정

--- dist/1.js
+++
@@ -1,6 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{53:function(e,t,n){"use strict";n.r(t);var s=n(0),o=n(23),a=n(24);window.customElements.define("page-404",class extends o.a{static get styles(){return[a.a]}get context(){return{title:"Page Not Found"}}render(){return s.d`
-      <section>
-        page not found (Page-404)
-      </section>
-    `}})}}]);
-//# sourceMappingURL=1.js.map
--- dist/2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{54:function(n,w){}}]);
-//# sourceMappingURL=2.js.map
